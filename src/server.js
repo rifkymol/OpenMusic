0e2ea38d92052b67f5ff84f155bc9eab76bf7d93
Rifkymoll <Rifkymaulana123@gmail.com>
2022-12-03 13:55:29 +0700

    Env implemented and adding file gitignore

--- src/server.js
+++ src/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config();
+
 const Hapi = require('@hapi/hapi');
 
 const albums = require('./api/albums');
@@ -14,8 +16,8 @@ const init = async () => {
   const songService = new SongsService();
 
   const server = Hapi.server({
-    port: 5000,
-    host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
+    port: process.env.PORT,
+    host: process.env.HOST,
     routes: {
       cors: {
         origin: ['*'],
